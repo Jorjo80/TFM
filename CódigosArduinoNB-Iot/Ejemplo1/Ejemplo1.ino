1626d799b39b3dd8f6a817cf9e9890ed537a5c6b
Jorjo80 <j_contri@hotmail.com>
2020-06-18 10:04:24 +0200

    Actualizaciones

--- CódigosArduinoNB-Iot/Ejemplo1/Ejemplo1.ino
+++ CódigosArduinoNB-Iot/Ejemplo1/Ejemplo1.ino
@@ -2,19 +2,46 @@
 
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(10, 11); 
-void setup() {
+
+void setup() 
+{
   Serial.begin(9600);
   while (!Serial) {
   }
   Serial.println("Wellcom to Dragino");
   mySerial.begin(9600);
+  initModem();  
 }
-void loop() { 
-  if (mySerial.available()) {
-    Serial.write(mySerial.read());
+
+void loop() 
+{   
+  if (mySerial.available()>0){
+      Serial.write(mySerial.read());
   }
-  if (Serial.available()) {
-    
+  if (Serial.available()) {    
     mySerial.write(Serial.read());
   }
+}
+ 
+
+void sendit(String in)
+{
+  //Serial.println(in+"\r");
+  Serial.println("\n");
+  mySerial.print(in+"\r");
+  delay(100);
+  while(mySerial.available() > 0){
+    if(mySerial.available() > 0){
+      byte c = mySerial.read();
+      Serial.write(c);
+    }
+  }
+}
+
+void initModem()
+{
+  sendit("ATE1");
+  delay(100);
+  sendit("ATI");
+  delay(100);
 }
