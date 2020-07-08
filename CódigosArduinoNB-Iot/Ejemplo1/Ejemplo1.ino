039206451c4df517b532bd0ccf1ff8ef759b30f5
Jorjo80 <j_contri@hotmail.com>
2020-07-08 23:47:20 +0200

    Codigos actualizados

--- CódigosArduinoNB-Iot/Ejemplo1/Ejemplo1.ino
+++ CódigosArduinoNB-Iot/Ejemplo1/Ejemplo1.ino
@@ -22,21 +22,66 @@ void loop()
     mySerial.write(Serial.read());
   }
 }
- 
+
+bool Respuesta(char letra, char msj)
+{
+  if(letra == msj)
+  {
+    return true;
+  }
+  else
+  {
+    //Serial.write("Mal");
+    return false;
+  }
+}
 
 void sendit(String in)
 {
   //Serial.println(in+"\r");
+  bool Prim = false;
+  bool Seg = false;
+  bool todo;
   Serial.println("\n");
   mySerial.print(in+"\r");
   delay(100);
-  while(mySerial.available() > 0){
-    if(mySerial.available() > 0){
-      byte c = mySerial.read();
+  while(mySerial.available() > 0)
+  {
+    if(mySerial.available() > 0)
+    {
+      char c = mySerial.read();
       Serial.write(c);
+      if(Prim == true)
+      {
+        Seg=Respuesta(c, 'K');
+      }
+      else
+      {
+        Prim = false;
+      }
+      
+      Prim=Respuesta(c,'O');
+      if(Seg == true)
+      {
+        todo = true;
+      }
+      else
+      {
+        todo = false;
+      }
+      if(todo == true)
+      {
+        Serial.write("\nRecibido Todo Bien");
+        todo = false;
+        Prim = false;
+        Seg = false;        
+      }
+     if(c== '\n')
+      Serial.write("fin");
     }
   }
 }
+
 
 void initModem()
 {
