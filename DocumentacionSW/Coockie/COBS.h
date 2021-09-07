b1409bc514a0340726fe94463fe25a1f4c2f688e
Jorge Contreras Ortiz <j_contri@hotmail.com>
2021-09-07 17:58:29 +0200

    Terminado SW

---
+++ DocumentacionSW/Coockie/COBS.h
@@ -0,0 +1,650 @@
+/****************************************************************************
+**                                                                         **
+**                         DEFINITIONS AND MACROS                          **
+**                                                                         **
+****************************************************************************/
+
+//En este documento se definen las funciones de codificación y decodificación COBS utilizadas
+
+// Se incluye comandos.h para integrar las variables uint8_t y uint16_t.
+
+#include "comandos.h"
+
+
+// Variables definidas por Kirale para sus funciones de codificación COBS
+
+#define COBS_SIZE_codS_ARRAY 10
+
+#define COBS_RESULT_NONE 0
+#define COBS_RESULT_ERROR -1
+#define COBS_RESULT_TIMEOUT -2
+
+/****************************************************************************
+**                                                                         **
+**                         TYPEDEFS AND STRUCTURES                         **
+**                                                                         **
+****************************************************************************/
+
+/* Encoded byte output function. */
+typedef void (*cobs_byteOut_t)(uint8_t c);
+
+/* Encoded byte input function. */
+typedef char ( *cobs_byteIn_t )( uint8_t *c);
+typedef char ( *cobs_byteIn_t2 )( uint8_t *c , uint8_t *m);
+
+//Estructuras utilizadas por las funciones de la codificación COBS	
+
+struct cobs_tx_s
+{
+	short pos[10];
+	char  cod[10];
+	char  codPos;
+};
+
+struct cobs_rx_s
+{
+	char dataBytes;
+	char zeroes;
+};
+
+/* Struct of transmission using COBS. */
+struct usart_tx_s
+{
+	short         totBytes; /* Total number of bytes to send. */
+	short         proBytes; /* Number of processed bytes. */
+	struct cobs_tx_s cobs;     /* COBS data. */
+};
+
+/* Struct of transmission using COBS. */
+struct usart_rx_s
+{
+	short         totBytes; /* Total number of bytes to send. */
+	short          proBytes; /* Number of bytes sent. */
+	char          startMsg;
+	char          payload;
+	struct cobs_rx_s cobs; /* COBS data. */
+};
+
+/****************************************************************************
+**                                                                         **
+**                            GLOBAL VARIABLES                             **
+**                                                                         **
+****************************************************************************/
+
+static struct usart_rx_s usart_rxPkt;
+
+/****************************************************************************
+**                                                                         **
+**                      PROTOTYPES OF LOCAL FUNCTIONS                      **
+**                                                                         **
+****************************************************************************/
+
+/*
+* Funcion Debug de los diferentes bytes que se transmiten o reciben.
+* Se muestran por pantalla a la vez que se transmiten o se reciben.
+*
+*  *** Argumentos ***
+*
+* _Bool tx --> Valor para diferenciar si es byte transmitido o recibido, Input
+* uint8_t --> Byte que se envía o se recibe, Input
+* _Bool first --> Valor para indicar el primer byte, Input
+* _Bool last --> Valor para indicar el último byte, Input
+*
+* Al utilizarse el mismo puerto UART en la Coockie para comunicación con el ordenador como con el KTWM102
+* se ha comentado/eliminado las llamadas a esta función para evitar el envío de mensajes inadecuados al módulo KTWM102
+*
+*/
+static void debug(_Bool tx, uint8_t byte, _Bool first, _Bool last);
+	   
+void  *memset (void *dest, int val, size_t len); 
+
+
+//#ifdef DEBUG_COBS
+void debug_tx( uint8_t byte, _Bool first, _Bool last ) {debug( 1, byte, first,  last );}
+void debug_rx( uint8_t byte, _Bool first, _Bool last ) {debug(0, byte, first,  last  );}
+//#else
+//#define debug_tx( uint8_t byte, _Bool first, _Bool last )( void ) 0
+//#define debug_rx(  uint8_t byte, _Bool first, _Bool last)( void ) 0
+//#endif
+
+
+
+/*
+* Función de Codificación COBS. Se utiliza función de librería "PacketSerial.h" para Arduino.
+*
+* *** Argumentos ***
+* const uint8_t* buffer --> Buffer que se desea codificar, Input
+* size_t size --> Valor del tamaño del buffer, Input
+* uint8_t* encoddBuffer	--> Buffer donde se guarda lo codificado, Output
+*
+*
+* Se retorna el tamaño de buffer resultante de la codificacion
+*
+*/
+static size_t encod(const uint8_t* buffer, size_t size, uint8_t* encoddBuffer)
+{
+		size_t read_index  = 0;
+		size_t write_index = 1;
+		size_t cod_index  = 0;
+		char cod       = 1;
+
+		while (read_index < size)
+		{
+				if (buffer[read_index] == 0)
+				{
+						encoddBuffer[cod_index] = cod;
+						//*encoddBuffer++ = cod;
+						cod = 1;
+						cod_index = write_index++;
+						read_index++;
+				}
+				else
+				{
+						encoddBuffer[write_index++] = buffer[read_index++];
+						/**encoddBuffer++ = *buffer++;
+						write_index++;
+						read_index++;*/	
+						cod++;
+
+						if (cod == 0xFF)
+						{
+								encoddBuffer[cod_index] = cod;
+								//*encoddBuffer++ = cod;
+								cod = 1;
+								cod_index = write_index++;
+						}
+				}
+		}
+
+		encoddBuffer[cod_index] = cod;
+		//*encoddBuffer++ = cod;
+		return write_index;
+}
+
+// COBS KIRALE
+
+
+
+/*int timeout(uint8_t inByte)
+{
+	debug_rx(inByte, 1, 1);
+	return COBS_RESULT_TIMEOUT;
+}
+
+ int nothing()
+{
+	return COBS_RESULT_NONE;
+}
+int first(uint8_t inByte)
+{
+	debug_rx(inByte, 1, 0);
+	return COBS_RESULT_NONE;
+}
+int error(uint8_t inByte)
+ {
+	debug_rx(inByte, 0, 1);
+	return COBS_RESULT_ERROR;
+}
+int incomplete(uint8_t inByte)
+{
+	debug_rx(inByte, 0, 0);
+	return COBS_RESULT_NONE;
+}
+int finished(uint8_t inByte)
+{
+	debug_rx(inByte, 0, 1);
+	return (usart_rxPkt.totBytes);
+}
+
+*/
+
+/* Función llamada por la codificación de Kirale, para enviar los datos por puerto UART
+* uint8_t byte --> Valor a enviar
+*/
+
+void sendChar(uint8_t byte)
+{
+	putchar(byte);
+}
+
+/*
+* Función Cobs_encod.
+* Función para la codificación creada por KIRALE. Actualmente NO esta 100% implantada, no esta adptada al código utlizado en el resto de la Coockie.
+* Al utilizarla, el envío de los datos no funciona correctamente y las comunicaciones con el módulo KTWM102 empiezan a fallar.
+* El error puede estar relacionado con el manejo de los punteros y posiciones de memoria de los bytes enviados.
+*
+*
+* *** Argumentos ***
+*
+* uint8_t *buff --> Buffer a codificar
+* uint16_t len --> Tamaño en bytes del Buffer
+* cobs_byteOut_t output --> Función llamada para el envío de los bytes codificados. En este caso "sendChar"
+* Se retorna el número de bytes codificados.
+
+*/
+
+
+int16_t cobs_encod(uint8_t *buff, uint16_t len, cobs_byteOut_t output)
+{
+	struct usart_tx_s usart_txPkt;
+	uint8_t *         tmpPtr = buff;
+	uint8_t *         lastZeroPtr = NULL;
+	uint8_t *         codPtr = NULL;
+	uint16_t          codPos = 0;
+	uint16_t          cod = 0x01;
+	uint8_t           numZeroes = 0;
+	int16_t           outIdx = 0;
+
+	/* Initialize COBS structure. */
+	memset(&usart_txPkt, 0, sizeof(struct usart_tx_s));
+	codPtr = usart_txPkt.cobs.cod;
+
+	/*
+	 * proBytes = number of encodd bytes
+	 * length   = Number of in data bytes to encod
+	 */
+	while (usart_txPkt.proBytes < len)
+	{
+		if (*tmpPtr == 0)
+		{
+			numZeroes++;
+			if ((numZeroes == 2) && (cod != 0x01))
+			{
+				if ((cod + 0xDF) <= 0xFE)
+				{
+					/* The (n-E0) data bytes, plus two trailing zeroes. */
+					cod += 0xDF;
+					numZeroes = 0;
+				}
+				else
+				{
+					/* The (n-1) data bytes followed by a single zero. */
+					numZeroes = 1;
+				}
+
+				*codPtr = cod;
+				usart_txPkt.totBytes++;
+				if (codPtr == usart_txPkt.cobs.cod + usart_txPkt.cobs.codPos)
+				{
+					usart_txPkt.cobs.pos[usart_txPkt.cobs.codPos] = codPos;
+					usart_txPkt.cobs.codPos++;
+				}
+
+				/* Reset counters. */
+				codPtr = tmpPtr;
+				codPos = usart_txPkt.proBytes;
+				cod = 0x01;
+			}
+			else if (numZeroes == 0x0F)
+			{
+				/* We have reached maximun number of zeroes in a row. */
+				cod = numZeroes + 0xD0;
+				codPtr = lastZeroPtr;
+				*codPtr = cod;
+				usart_txPkt.totBytes++;
+
+				/* Reset counters. */
+				codPtr = tmpPtr;
+				codPos = usart_txPkt.proBytes;
+				numZeroes = 0;
+				cod = 0x01;
+			}
+			else if ((codPtr ==
+				usart_txPkt.cobs.cod + usart_txPkt.cobs.codPos) &&
+				(cod == 0x01) && (numZeroes == 1))
+			{
+				codPtr = tmpPtr;
+				codPos = usart_txPkt.proBytes;
+			}
+
+			lastZeroPtr = tmpPtr;
+		}
+		else if (numZeroes)
+		{
+			/* Finish previous block. */
+			if (numZeroes < 3)
+			{
+				if (cod == 0x01)
+				{
+					if (numZeroes == 2)
+					{
+						/* The (n-E0) data bytes, plus two trailing zeroes. */
+						cod += 0xDF;
+					}
+					else
+					{
+						/* The (n-1) data bytes followed by a single zero. */
+						/* Nothing to do. */
+					}
+
+					*codPtr = cod;
+					usart_txPkt.totBytes++;
+
+					if (codPtr != lastZeroPtr)
+					{
+						codPtr = lastZeroPtr;
+						codPos = usart_txPkt.proBytes - 1;
+					}
+					else
+					{
+						/* Add aditional byte. */
+						codPtr = usart_txPkt.cobs.cod + usart_txPkt.cobs.codPos;
+						codPos = usart_txPkt.proBytes;
+					}
+				}
+				else
+				{
+					/* The (n-1) data bytes followed by a single zero. */
+					*codPtr = cod;
+					usart_txPkt.totBytes++;
+
+					if (codPtr == usart_txPkt.cobs.cod + usart_txPkt.cobs.codPos)
+					{
+						usart_txPkt.cobs.pos[usart_txPkt.cobs.codPos] = codPos;
+						usart_txPkt.cobs.codPos++;
+					}
+
+					codPtr = lastZeroPtr;
+					codPos = usart_txPkt.proBytes - 1;
+				}
+			}
+			else
+			{
+				/* A run of (n-D0) zeroes. */
+				cod = numZeroes + 0xD0;
+				*codPtr = cod;
+				usart_txPkt.totBytes++;
+				codPtr = lastZeroPtr;
+				codPos = usart_txPkt.proBytes - 1;
+			}
+
+			/* Reset counters. */
+			numZeroes = 0;
+			cod = 0x02;
+			usart_txPkt.totBytes++;
+		}
+		else
+		{
+			/* Increment cod. */
+			cod++;
+			if (cod == 0xD0)
+			{
+				/*
+				 * We have reached the maximum number of bytes not followed by a
+				 * zero.
+				 */
+				*codPtr = 0xD0;
+				usart_txPkt.totBytes++;
+				if (codPtr == usart_txPkt.cobs.cod + usart_txPkt.cobs.codPos)
+				{
+					usart_txPkt.cobs.pos[usart_txPkt.cobs.codPos] = codPos;
+					usart_txPkt.cobs.codPos++;
+				}
+
+				/* Add aditional byte.*/
+				codPtr = usart_txPkt.cobs.cod + usart_txPkt.cobs.codPos;
+				codPos = usart_txPkt.proBytes + 1;
+
+				/* Reset counters. */
+				cod = 0x01;
+			}
+
+			usart_txPkt.totBytes++;
+		}
+
+		/* Increment pointers. */
+		tmpPtr++;
+		usart_txPkt.proBytes++;
+	}
+
+	/* Finish message. */
+	numZeroes++;
+	if (numZeroes == 2)
+	{
+		if ((cod + 0xDF) <= 0xFE)
+		{
+			/* The (n-E0) data bytes, plus two trailing zeroes. */
+			cod += 0xDF;
+		}
+		else
+		{
+			/* The (n-1) data bytes followed by a single zero. */
+			*codPtr = cod;
+			usart_txPkt.totBytes++;
+			if (codPtr == usart_txPkt.cobs.cod + usart_txPkt.cobs.codPos)
+			{
+				usart_txPkt.cobs.pos[usart_txPkt.cobs.codPos] = codPos;
+				usart_txPkt.cobs.codPos++;
+			}
+
+			codPtr = lastZeroPtr;
+			cod = 0x01;
+		}
+	}
+	else if (numZeroes > 2)
+	{
+		/* A run of zeroes. */
+		cod = numZeroes + 0xD0;
+	}
+
+	*codPtr = cod;
+	usart_txPkt.totBytes++;
+	if (codPtr == usart_txPkt.cobs.cod + usart_txPkt.cobs.codPos)
+		usart_txPkt.cobs.pos[usart_txPkt.cobs.codPos] = codPos;
+
+	/* Send out encodd bytes through the callback */
+	usart_txPkt.proBytes = 0;
+	usart_txPkt.cobs.codPos = 0;
+	while (outIdx < usart_txPkt.totBytes + 1)
+	{
+		if (outIdx == 0)
+		{
+			/* First zero as start delimiter */
+			debug_tx(0x00, 1, outIdx == usart_txPkt.totBytes);
+			output(0x00);
+			outIdx++;
+		}
+		else if ((usart_txPkt.cobs.pos[usart_txPkt.cobs.codPos] ==
+			usart_txPkt.proBytes) &&
+			(usart_txPkt.cobs.cod[usart_txPkt.cobs.codPos] != 0))
+		{
+			debug_tx(usart_txPkt.cobs.cod[usart_txPkt.cobs.codPos], 0, outIdx == usart_txPkt.totBytes);
+			output(usart_txPkt.cobs.cod[usart_txPkt.cobs.codPos]);
+			usart_txPkt.cobs.codPos++;
+			outIdx++;
+		}
+		else
+		{
+			uint8_t dato = 0;
+
+			while (dato == 0)
+			{
+				dato = buff[usart_txPkt.proBytes++];
+				if (dato != 0)
+				{
+					debug_tx(dato, 0, outIdx == usart_txPkt.totBytes);
+					output(dato);
+					outIdx++;
+				}
+			}
+		}
+	}
+
+	return (outIdx);
+}
+
+
+/*
+
+* Función realizada por Kirale que implementa la decodificación de los carácteres que se reciben por puerto UART
+* Se ha debido adaptar. Inicialmente, decodificaba en tiempo real, según recibe un byte, lo decodifica. 
+* Pyesto que la Coockie no era capaz de procesar y decodificar los bytes a la velocidad que llegaban los bytes
+* Se ha adaptado para ser llamada una vez se ha leido la respuesta del KTWM102.
+* 
+* Decodificación byte a byte
+* 
+* *** Argumentos ***
+* 
+*	uint8_t *buff -->  Buffer donde se almacena la cadena decodificada
+*   uint16_t len  -->  Longitud máxima que se espera decodificar.
+*   uint8_t input -->  Es el byte a codificar
+*
+* Se retorna:
+* 0 si aún no ha terminado de decodificar
+* -1 si ha dado un error
+* Número >0 cuando ha finalizado la decodificación indicando el número de bytes decodificados
+
+*/
+
+
+int16_t cobs_decod(uint8_t *buff, uint16_t len, uint8_t input)
+{
+	uint8_t inByte = 0;
+	uint8_t numChar = 0;
+	inByte = input;
+	numChar = 1;
+	if (numChar == 0)
+	{
+		//debug_rx(inByte, 1, 1);
+		return COBS_RESULT_TIMEOUT;
+	}
+	if (inByte == 0)
+	{
+		uint16_t dataBytes = usart_rxPkt.cobs.dataBytes;
+		/* Initialize COBS structure. */
+		usart_rxPkt.totBytes = 5;
+		usart_rxPkt.proBytes = 0;
+		usart_rxPkt.startMsg = 1;
+		usart_rxPkt.payload = 0;
+		usart_rxPkt.cobs.dataBytes = 0;
+		usart_rxPkt.cobs.zeroes = 0;
+		memset(buff, 0, 5);
+		if (dataBytes == 0)
+		{
+			//debug_rx(inByte, 1, 0);
+			return COBS_RESULT_NONE;
+		}
+		else
+		{
+			return COBS_RESULT_NONE;
+		}
+	}
+	else if ((inByte != 0) && (usart_rxPkt.startMsg == 1))
+	{
+		if ((usart_rxPkt.proBytes >= 2) && (usart_rxPkt.payload == 0))
+		{
+			usart_rxPkt.totBytes += (buff[0] << 8) + buff[1];
+			if (usart_rxPkt.totBytes > len)
+			{
+				//debug_rx(inByte, 0, 1);
+				return COBS_RESULT_ERROR;
+			}
+
+			memset(buff + 5, 0, usart_rxPkt.totBytes - 5);
+			usart_rxPkt.payload = 1;
+		}
+
+		if (usart_rxPkt.cobs.dataBytes == 0)
+		{
+			/* Read COBS cod. */
+			if (inByte < 0xD0)
+			{
+				usart_rxPkt.cobs.dataBytes = inByte - 1;
+				usart_rxPkt.cobs.zeroes = 1;
+			}
+			else if (inByte == 0xD0)
+			{
+				usart_rxPkt.cobs.dataBytes = inByte - 1;
+				usart_rxPkt.cobs.zeroes = 0;
+			}
+			else if ((inByte == 0xD1) || (inByte == 0xD2))
+			{
+				//debug_rx(inByte, 0, 1);
+				return COBS_RESULT_ERROR;
+			}
+			else if (inByte < 0xE0)
+			{
+				/* Move pointer to the new position. */
+				usart_rxPkt.cobs.dataBytes = 0;
+				usart_rxPkt.cobs.zeroes = inByte - 0xD0;
+			}
+			else if (inByte < 0xFF)
+			{
+				usart_rxPkt.cobs.dataBytes = inByte - 0xE0;
+				usart_rxPkt.cobs.zeroes = 2;
+			}
+			else
+			{
+				//debug_rx(inByte, 0, 1);
+				return COBS_RESULT_ERROR;
+			}
+
+			if (usart_rxPkt.cobs.dataBytes == 0)
+			{
+				usart_rxPkt.proBytes += usart_rxPkt.cobs.zeroes;
+				usart_rxPkt.cobs.zeroes = 0;
+			}
+		}
+		else
+		{
+			if (usart_rxPkt.proBytes < usart_rxPkt.totBytes)
+			{
+				/* Read data byte. */
+				buff[usart_rxPkt.proBytes] = inByte;
+			}
+
+			++usart_rxPkt.proBytes;
+			if (--usart_rxPkt.cobs.dataBytes == 0)
+			{
+				usart_rxPkt.proBytes += usart_rxPkt.cobs.zeroes;
+				usart_rxPkt.cobs.zeroes = 0;
+			}
+		}
+	}
+	else
+	{
+		return COBS_RESULT_NONE;
+	}
+
+	if (usart_rxPkt.proBytes >= usart_rxPkt.totBytes)
+	{
+		usart_rxPkt.startMsg = 0;
+		//debug_rx(inByte, 0, 0);
+		return COBS_RESULT_NONE;
+	}
+	else
+	{
+		//debug_rx(inByte, 0, 1);
+		return (usart_rxPkt.totBytes);
+	}
+
+
+
+}
+
+
+ static void debug(_Bool tx, uint8_t byte, _Bool first, _Bool last)
+{
+	if (first)
+		printf("");
+
+	if (!(first && last))
+		printf("");
+
+	if (last)
+		printf("");
+	else
+		;
+
+}
+
+
+void  *memset (void *dest, int val, size_t len)
+{
+  unsigned char *ptr = dest;
+  while (len-- > 0)
+    *ptr++ = val;
+  return dest;
+}
+
+
---
+++ DocumentacionSW/STM/MDK-ARM/COBS.h
@@ -0,0 +1,349 @@
+//En este documento están declaradas las estructuras y funciones necesarias para codificación y decodificación COBS
+
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+#include <inttypes.h>
+
+/****************************************************************************
+**                                                                         **
+**                         DEFINITIONS AND MACROS                          **
+**                                                                         **
+****************************************************************************/
+
+#define COBS_SIZE_CODES_ARRAY 10
+
+#define COBS_RESULT_NONE 0
+#define COBS_RESULT_ERROR -1
+#define COBS_RESULT_TIMEOUT -2
+
+/****************************************************************************
+**                                                                         **
+**                         TYPEDEFS AND STRUCTURES                         **
+**                                                                         **
+****************************************************************************/
+
+/* Encoded byte output function. */
+typedef void ( *cobs_byteOut_t )( uint8_t c, UART_HandleTypeDef *m );
+
+/* Encoded byte input function. */
+typedef uint8_t ( *cobs_byteIn_t )( uint8_t *c , UART_HandleTypeDef *m);
+typedef uint8_t ( *cobs_byteIn_t2 )( uint8_t *c , uint8_t *m);
+	
+
+struct cobs_tx_s
+{
+	uint16_t pos[COBS_SIZE_CODES_ARRAY];
+	uint8_t  code[COBS_SIZE_CODES_ARRAY];
+	uint8_t  codePos;
+};
+
+struct cobs_rx_s
+{
+	uint8_t dataBytes;
+	uint8_t zeroes;
+};
+
+/* Struct of transmission using COBS. */
+struct usart_tx_s
+{
+	uint16_t         totBytes; /* Total number of bytes to send. */
+	uint16_t         proBytes; /* Number of processed bytes. */
+	struct cobs_tx_s cobs;     /* COBS data. */
+};
+
+/* Struct of transmission using COBS. */
+struct usart_rx_s
+{
+	uint16_t         totBytes; /* Total number of bytes to send. */
+	int16_t          proBytes; /* Number of bytes sent. */
+	uint8_t          startMsg;
+	uint8_t          payload;
+	struct cobs_rx_s cobs; /* COBS data. */
+};
+
+/****************************************************************************
+**                                                                         **
+**                            GLOBAL VARIABLES                             **
+**                                                                         **
+****************************************************************************/
+
+static struct usart_rx_s usart_rxPkt;
+
+/****************************************************************************
+**                                                                         **
+**                      PROTOTYPES OF LOCAL FUNCTIONS                      **
+**                                                                         **
+****************************************************************************/
+
+/*
+
+* Funcion Debug de los diferentes bytes que se transmiten o reciben.
+* Se muestran por pantalla a la vez que se transmiten o se reciben.
+* 
+* Argumentos
+* 
+* _Bool tx --> Valor para diferenciar si es byte transmitido o recibido, Input
+* uint8_t --> Byte que se envía o se recibe, Input
+* _Bool first --> Valor para indicar el primer byte, Input
+* _Bool last --> Valor para indicar el último byte, Input
+*
+*/
+
+
+
+static void debug(_Bool tx, uint8_t byte, _Bool first, _Bool last);
+
+#ifdef DEBUG_COBS
+#define debug_tx( ... ) debug( 1, __VA_ARGS__ )
+#define debug_rx( ... ) debug( 0, __VA_ARGS__ )
+#else
+#define debug_tx( ... ) ( void ) 0
+#define debug_rx( ... ) ( void ) 0
+#endif /* DEBUG_COBS*/
+
+/******************* COBS ENCODE PROPIO *******************/
+
+/*
+
+* Funcion getEncodedBufferSize. Se obtiene de libreria PacketSerial.h (Arduino)
+*
+* Se calcula el tamaño del buffer donde se almacenará el comando codificado
+* 
+* Argumentos
+*
+* size_t unencodedBufferSize --> Tamaño en bytes del comando a codificar
+* 
+* Se retorna el tamaño del buffer donde se almacenará el comando codificado
+*
+*/
+
+static size_t getEncodedBufferSize(size_t unencodedBufferSize)
+{
+		return unencodedBufferSize + unencodedBufferSize / 254 + 1;
+}
+
+
+/*
+
+* Funcion encode. Se obtiene de libreria PacketSerial.h (Arduino)
+*
+* Se codifica el comando deseado
+* 
+* Argumentos
+*
+* uint8_t* buffer --> Comando a codificar
+* size_t size --> Tamaño del comando a codificar
+* uint8_t* encodedBuffer --> Buffer donde se almacenará el comando codificado
+* 
+* Se retorna el número de bytes codificados
+*
+*/
+
+static size_t encode(const uint8_t* buffer, size_t size, uint8_t* encodedBuffer)
+{
+		size_t read_index  = 0;
+		size_t write_index = 1;
+		size_t code_index  = 0;
+		uint8_t code       = 1;
+
+		while (read_index < size)
+		{
+				if (buffer[read_index] == 0)
+				{
+						encodedBuffer[code_index] = code;
+						code = 1;
+						code_index = write_index++;
+						read_index++;
+				}
+				else
+				{
+						encodedBuffer[write_index++] = buffer[read_index++];
+						code++;
+
+						if (code == 0xFF)
+						{
+								encodedBuffer[code_index] = code;
+								code = 1;
+								code_index = write_index++;
+						}
+				}
+		}
+
+		encodedBuffer[code_index] = code;
+
+		return write_index;
+}
+
+
+/******************* COBS DECODE KIRALE *******************/
+
+/*
+* Función cobs_decode
+*
+* Función creada por KIRALE para la decodificación
+* 
+* Ha sido adaptada para las comunicaciones UART del módulo utilizado.
+*
+* Decodificación en tiempo real de las respuestas del módulo KTWM102. Decodifica byte a byte según se van recibiendo
+* 
+* Argumentos
+* 
+*		uint8_t *buff -->  Buffer donde se almacena la cadena decodificada
+* 	uint16_t len  -->  Longitud máxima que se espera decodificar.
+* 	cobs_byteIn_t input -->  Función a la que se llamará para la lectura de los bytes entrantes
+*   UART_HandleTypeDef *modulo --> Entrada UART de la que leer los bytes.
+*
+* Se retorna:
+* 	0 si aún no ha terminado de decodificar
+* 	-1 si ha dado un error
+* 	Número >0 cuando ha finalizado la decodificación indicando el número de bytes decodificados
+*
+*/
+
+
+
+int16_t cobs_decode(uint8_t *buff, uint16_t len, cobs_byteIn_t input, UART_HandleTypeDef *modulo)
+{
+	uint8_t inByte = 0;
+	uint8_t numChar = 0;
+	numChar = input(&inByte, modulo);
+	if (numChar == 0)
+		goto timeout;
+
+	if (inByte == 0)
+	{
+		uint16_t dataBytes = usart_rxPkt.cobs.dataBytes;
+		/* Initialize COBS structure. */
+		usart_rxPkt.totBytes = 5;
+		usart_rxPkt.proBytes = 0;
+		usart_rxPkt.startMsg = 1;
+		usart_rxPkt.payload = 0;
+		usart_rxPkt.cobs.dataBytes = 0;
+		usart_rxPkt.cobs.zeroes = 0;
+		memset(buff, 0, 5);
+		if (dataBytes == 0)
+			goto first;
+		else
+			goto nothing;
+	}
+	else if ((inByte != 0) && (usart_rxPkt.startMsg == 1))
+	{
+		if ((usart_rxPkt.proBytes >= 2) && (usart_rxPkt.payload == 0))
+		{
+			usart_rxPkt.totBytes += (buff[0] << 8) + buff[1];
+			if (usart_rxPkt.totBytes > len)
+				goto error;
+
+			memset(buff + 5, 0, usart_rxPkt.totBytes - 5);
+			usart_rxPkt.payload = 1;
+		}
+
+		if (usart_rxPkt.cobs.dataBytes == 0)
+		{
+			/* Read COBS code. */
+			if (inByte < 0xD0)
+			{
+				usart_rxPkt.cobs.dataBytes = inByte - 1;
+				usart_rxPkt.cobs.zeroes = 1;
+			}
+			else if (inByte == 0xD0)
+			{
+				usart_rxPkt.cobs.dataBytes = inByte - 1;
+				usart_rxPkt.cobs.zeroes = 0;
+			}
+			else if ((inByte == 0xD1) || (inByte == 0xD2))
+				goto error;
+			else if (inByte < 0xE0)
+			{
+				/* Move pointer to the new position. */
+				usart_rxPkt.cobs.dataBytes = 0;
+				usart_rxPkt.cobs.zeroes = inByte - 0xD0;
+			}
+			else if (inByte < 0xFF)
+			{
+				usart_rxPkt.cobs.dataBytes = inByte - 0xE0;
+				usart_rxPkt.cobs.zeroes = 2;
+			}
+			else
+				goto error;
+
+			if (usart_rxPkt.cobs.dataBytes == 0)
+			{
+				usart_rxPkt.proBytes += usart_rxPkt.cobs.zeroes;
+				usart_rxPkt.cobs.zeroes = 0;
+			}
+		}
+		else
+		{
+			if (usart_rxPkt.proBytes < usart_rxPkt.totBytes)
+			{
+				/* Read data byte. */
+				buff[usart_rxPkt.proBytes] = inByte;
+			}
+
+			++usart_rxPkt.proBytes;
+			if (--usart_rxPkt.cobs.dataBytes == 0)
+			{
+				usart_rxPkt.proBytes += usart_rxPkt.cobs.zeroes;
+				usart_rxPkt.cobs.zeroes = 0;
+			}
+		}
+	}
+	else
+		goto nothing;
+
+	if (usart_rxPkt.proBytes >= usart_rxPkt.totBytes)
+	{
+		usart_rxPkt.startMsg = 0;
+		goto finished;
+	}
+	else
+		goto incomplete;
+
+	
+/*
+*	ETIQUETAS
+*	
+* Utilizadas en cobs_decode para debuguear los bytes recibidos
+* Según el caso donde se entre, se finalizará la función de decodificar, se devolverá error o se continuará decodificando	
+*	
+*/	
+	
+timeout:
+	debug_rx(inByte, 1, 1);
+	return COBS_RESULT_TIMEOUT;
+nothing:
+	return COBS_RESULT_NONE;
+first:
+	debug_rx(inByte, 1, 0);
+	return COBS_RESULT_NONE;
+error:
+	debug_rx(inByte, 0, 1);
+	return COBS_RESULT_ERROR;
+incomplete:
+	debug_rx(inByte, 0, 0);
+	return COBS_RESULT_NONE;
+finished:
+	debug_rx(inByte, 0, 1);
+	return (usart_rxPkt.totBytes);
+}
+
+
+static void debug(_Bool tx, uint8_t byte, _Bool first, _Bool last)
+{
+	if (first)
+		printf("COBS_%sX: |", tx ? "T" : "R");
+
+	if (!(first && last))
+		printf(" %02x ", byte);
+
+	if (last)
+		printf("|\n");
+	else
+		printf(":");
+}
+
